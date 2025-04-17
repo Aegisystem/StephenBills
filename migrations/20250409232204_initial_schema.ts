c9520baa10e70bf730010b244b93cd354e46b2b6
Steven Correa <steven14correa@gmail.com>
2025-04-16 19:01:21 -0500

    Added attributes in customers schema

--- migrations/20250409232204_initial_schema.ts
+++ migrations/20250409232204_initial_schema.ts
@@ -1,21 +1,23 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('customers', (table) => {
+  await knex.schema.createTable('customers', (table: Knex.TableBuilder) => {
     table.bigIncrements('id').primary();
     table.string('document_id', 20).notNullable().unique();
     table.string('document_type', 10).notNullable();
+    table.integer('verification_digit', 1).notNullable();
     table.string('first_name', 100);
     table.string('last_name', 100);
     table.string('company_name', 100);
     table.string('email', 100).notNullable();
-    table.string('phone', 20).notNullable();
+    table.string('phone', 20);
     table.jsonb('address');
+    table.string('person_type').notNullable()
     table.boolean('is_consortium').defaultTo(false);
     table.timestamps(true, true);
   });
 
-  await knex.schema.createTable('entries', (table) => {
+  await knex.schema.createTable('entries', (table: Knex.TableBuilder) => {
     table.bigIncrements('id').primary();
     table.bigInteger('owner_id').notNullable();
     table.string('concept', 50).notNullable();
