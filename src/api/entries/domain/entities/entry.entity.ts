7dbfebcbaecb8aac57fe3fd69a34d33d58aeccee
Steven Correa <steven14correa@gmail.com>
2025-06-26 11:31:18 -0500

    Fix on camelCase entry entity

--- src/api/entries/domain/entities/entry.entity.ts
+++ src/api/entries/domain/entities/entry.entity.ts
@@ -1,13 +1,11 @@
-import { PersonType } from '../../../shared/domain/person-type.enum';
-
 export class Entry {
   public readonly id: bigint;
   public readonly cufe: string;
-  public owner_id: bigint;
-  public third_party_id?: bigint;
+  public ownerId: bigint;
+  public thirdPartyId?: bigint;
   public concept: string;
   public debit: number;
-  public issue_date?: string;
+  public issueDate?: string;
   public createdAt: Date;
   public updatedAt: Date;
 }
